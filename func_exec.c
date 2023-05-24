fd460fb80fc29b7e09c6ea1cb447a2a96acd6496
mosesgitonga <infosec947@gmail.com>
2023-05-24 05:29:59 -0700

    bety

--- func_exec.c
+++ func_exec.c
@@ -10,40 +10,39 @@
  */
 void execute_instructions(char *file_name)
 {
-    FILE *fd;
-    char line[100];
-    unsigned int counter = 0;
-    char *op;
-    struct instruction_s instruction;
+	FILE *fd;
+	char line[100];
+	unsigned int counter = 0;
+	char *op;
+	struct instruction_s instruction;
 
-    fd = fopen(file_name, "r");
-    if (fd == NULL)
+	fd = fopen(file_name, "r");
+	if (fd == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", file_name);
 		exit(EXIT_FAILURE);
 	}
-    while (fgets(line, sizeof(line), fd))
-    {
-        counter++;
-        op = strtok_custom(line, " \t\n$");
-        if (op == NULL)
-            continue;
-        instruction.opcode = op;
-
-        if (strcmp(op, "push") == 0)
-            instruction.f = push;
-        else if (strcmp(op, "pall") == 0)
-            instruction.f = pall;
-        else if (strcmp(op, "pint") == 0)
-            instruction.f = pint;
-        else if (strcmp(op, "pop") == 0)
-            instruction.f = pop;
+	while (fgets(line, sizeof(line), fd))
+	{
+		counter++;
+		op = strtok_custom(line, " \t\n$");
+		if (op == NULL)
+		continue;
+			instruction.opcode = op;
+		if (strcmp(op, "push") == 0)
+			instruction.f = push;
+		else if (strcmp(op, "pall") == 0)
+			instruction.f = pall;
+		else if (strcmp(op, "pint") == 0)
+			instruction.f = pint;
+		else if (strcmp(op, "pop") == 0)
+			instruction.f = pop;
 		else if (strcmp(op, "swap") == 0)
 			instruction.f = swap;
-        else
-            printf("L%d: unknown instruction %s\n", counter, op);
-        instruction.f(&head, counter);
-    }
-    fclose(fd);
+		else
+			printf("L%d: unknown instruction %s\n", counter, op);
+		instruction.f(&head, counter);
+	}
+	fclose(fd);
 }
 
