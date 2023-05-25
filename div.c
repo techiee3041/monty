ce95d71416af98b1fe7402024a3ff664ad73d6b2
techiee3041 <doreenoms6699@gmail.com>
2023-05-25 21:05:01 +0300

    betty

--- div.c
+++ div.c
@@ -7,17 +7,16 @@ void div_func(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't div, stack too short", counter);
 		exit(EXIT_FAILURE);
 	}	
-	
+
 	if ((*head)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero", counter);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	(*head)->next->n /= (*head)->n;
 
 
 	pop(head, counter);
 
- 
 }
