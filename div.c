9afee4526d9af26ca8421c81f37874407e72cae2
moses gitonga <infosec947@fmail.com>
2023-05-26 07:10:02 +0300

    merge

--- div.c
+++ div.c
@@ -4,13 +4,13 @@ void div_func(stack_t **head, unsigned int counter)
 {
 	if (*head == NULL || (*head)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short", counter);
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
 		exit(EXIT_FAILURE);
 	}	
 	
 	if ((*head)->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero", counter);
+		fprintf(stderr, "L%d: division by zero\n", counter);
 		exit(EXIT_FAILURE);
 	}
 	
